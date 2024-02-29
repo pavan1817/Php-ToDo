7b98860de054f5016c81ea8e6c6cff512db422a5
Pavan <92082258+pavan1817@users.noreply.github.com>
2024-02-29 10:14:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDo List</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
 </head>
 <body>
     <?php
@@ -14,7 +16,7 @@
     ?>
     <div class="container my-5">
         <h2>To-Do List</h2>
-        <a class="btn btn-primary" href="/addTask.php" role="button">Add Task</a>
+        <a class="btn btn-primary" href="/addTask.php" role="button"><i class="bi bi-plus-lg"></i> Add Task</a>
         <br>
         <table class="table">
             <thead>
@@ -22,7 +24,7 @@
                     <th>ID</th>
                     <th>Title</th>
                     <th>Description</th>
-                    <th>Completed</th>
+                    <th>Status</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -41,18 +43,29 @@
                 if(!$result){
                     die("Invalid query: " . $conn->error);
                 }
-                print_r($result);
+                // print_r($result);
 
                 while($row = $result->fetch_assoc()){
+
+                    // button text based on the completion status
+                    $completedButtonText = $row['status'] ? 'Completed' : 'Mark as Completed';
+                    // action based on the completion status
+                    $completedButtonAction = $row['status'] ? 'btn-secondary' : 'btn-success';
+                    // Determine the link for toggling completion
+                    $toggleCompletionLink = $row['status'] ? "/completion.php?id=$row[id]&status=0" : "/completion.php?id=$row[id]&status=1";
+
+                    
                     echo "
                     <tr>
                     <td>$row[id]</td>
                     <td>$row[title]</td>
                     <td>$row[description]</td>
-                    <td>$row[completed]</td>
                     <td>
-                        <a class='btn btn-primary btn-sm' href='/edit.php?id=$row[id]'>Edit</a>
-                        <a class='btn btn-danger btn-sm' href='/delete.php?id=$row[id]'>Delete</a>
+                        <a class='btn btn-sm $completedButtonAction' href='$toggleCompletionLink'>$completedButtonText</a>
+                    </td>
+                    <td>
+                        <a href='/edit.php?id=$row[id]'><i class='bi bi-pencil-square' style='color: #3545dc; margin-right: 15px;'></i></a>
+                        <a href='/delete.php?id=$row[id]'><i class='bi bi-trash3-fill' style='color: #dc3545;'></i></a>
                     </td>
                 </tr>
                     ";
