9d1a676a89c61e8e96819f7c50935c9df8b2a18c
Pavan <92082258+pavan1817@users.noreply.github.com>
2024-03-01 12:04:38 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,31 +2,23 @@
 
 include 'dbconn.php';
 
-// Determine the ID to be deleted
+// id to be deleted
 $delete_id = $_GET['id'];
 
-// Delete the row from the table
-$sql_delete_id = "DELETE FROM tasks WHERE id = $delete_id";
+$current_datetime = date('Y-m-d H:i:s');
 
-if ($conn->query($sql_delete_id) === TRUE) {
-    // Fetch the IDs of all rows in the table
-    $sql_select = "SELECT id FROM tasks ORDER BY id";
-    $result = $conn->query($sql_select);
+// updating the row from the table
+$sql_update = "UPDATE tasks SET deleted = TRUE, deleted_at = '$current_datetime' WHERE id = $delete_id";
 
-    // Update the IDs of the rows that come after the deleted row
-    $new_id = 1;
-    while ($row = $result->fetch_assoc()) {
-        $current_id = $row['id'];
-        $sql_update = "UPDATE tasks SET id = $new_id WHERE id = $current_id";
-        $conn->query($sql_update);
-        $new_id++;
-    }
-
+if ($conn->query($sql_update) === TRUE) {
     header("location: /index.php");
     exit;
 } else {
-    echo "Error deleting record: " . $conn->error;
+    echo "Error:" . $conn->error;
 }
 
+// $sql = "SELECT * FROM tasks WHERE deleted = FALSE";
+// $result = $conn->query($sql);
+
 $conn->close();
-?>
+?> 
